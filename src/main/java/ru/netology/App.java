f41a115b10bf28a240bbe400949063336ae6be29
teodorro <edem.teo@gmail.com>
2020-07-27 18:50:09 +0300

    .

--- src/main/java/ru/netology/App.java
+++ src/main/java/ru/netology/App.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import api.Main;
 
 /**
  * Hello world!
@@ -10,7 +9,5 @@ public class App
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello World!" );
-        Main.main(new String[1]);
     }
 }
